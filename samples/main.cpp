b949f65cf2d08e2753d173ff490b3836cd71f935
Sébastien NEDJAR <nedseb@goldaire.com>
2018-11-06 14:39:11 +0200

    Update samples

--- samples/main.cpp
+++ samples/main.cpp
@@ -1,8 +1,46 @@
 #include "stm32.h"
 #include "STM32IotNode.h"
 
-#include "Blink.h"
-#include "TemperatureAlarm.h"
+#if defined(BLE_TEMPERATURE_ALARM_SAMPLE)
+    #include "TemperatureAlarm.h"
+    #define SAMPLE_MAIN TemperatureAlarm_main
+#elif defined(HTS221_SAMPLE)
+    #include "HTS221.h"
+    #define SAMPLE_MAIN HTS221_main
+#elif defined(LIS3MDL_SAMPLE)
+    #include "LIS3MDL.h"
+    #define SAMPLE_MAIN LIS3MDL_main
+#elif defined(LSM6DSL_SAMPLE)
+    #include "LSM6DSL.h"
+    #define SAMPLE_MAIN LSM6DSL_main
+#elif defined(LSM6DSL_GYRO_SAMPLE)
+    #include "LSM6DSLGyro.h"
+    #define SAMPLE_MAIN LSM6DSLGyro_main
+#elif defined(LSM6DSL_ACCELERO_SAMPLE)
+    #include "LSM6DSLAccelero.h"
+    #define SAMPLE_MAIN LSM6DSLAccelero_main
+#elif defined(LPS22HB_SAMPLE)
+    #include "LPS22HB.h"
+    #define SAMPLE_MAIN LPS22HB_main
+#elif defined(VL53L0X_SAMPLE)
+    #include "VL53L0X.h"
+    #define SAMPLE_MAIN VL53L0X_main
+#elif defined(BUTTON_SAMPLE)
+    #include "Button.h"
+    #define SAMPLE_MAIN BUTTON_main
+#elif defined(GPIO_SAMPLE)
+    #include "GPIO.h"
+    #define SAMPLE_MAIN GPIO_main
+#elif defined(ADC_SAMPLE)
+    #include "ADC.h"
+    #define SAMPLE_MAIN ADC_main
+#elif defined(FLASH_MEMORY_SAMPLE)
+    #include "FlashMemory.h"
+    #define SAMPLE_MAIN FlashMemory_main
+#else
+    #include "Blink.h"
+    #define SAMPLE_MAIN Blink_main
+#endif
 
 using namespace codal;
 STM32IotNode iotNode;
@@ -10,31 +48,5 @@ STM32IotNode iotNode;
 int main()
 {
     iotNode.init();
-    #if defined(BLE_TEMPERATURE_ALARM_SAMPLE)
-    TemperatureAlarm_main(iotNode);
-    #elif defined(HTS221_SAMPLE)
-    HTS221_main(iotNode)
-    #elif defined(LIS3MDL_SAMPLE)
-    LIS3MDL_main(iotNode)
-    #elif defined(LSM6DSL_SAMPLE)
-    LSM6DSL_main(iotNode)
-    #elif defined(LSM6DSL_GYRO_SAMPLE)
-    LSM6DSLGyro_main(iotNode)
-    #elif defined(LSM6DSL_ACCELERO_SAMPLE)
-    LSM6DSLAccelero_main(iotNode)
-    #elif defined(LPS22HB_SAMPLE)
-    LPS22HB_main(iotNode)
-    #elif defined(VL53L0X_SAMPLE)
-    _main(iotNode)
-    #elif defined(BUTTON_SAMPLE)
-    BUTTON_main(iotNode)
-    #elif defined(GPIO_SAMPLE)
-    GPIO_main(iotNode)
-    #elif defined(ADC_SAMPLE)
-    ADC_main(iotNode)
-    #elif defined(FLASH_MEMORY_SAMPLE)
-    FLASH_MEMORY_main(iotNode)
-    #else
-    Blink_main(iotNode);
-    #endif
+    SAMPLE_MAIN(iotNode);
 }
