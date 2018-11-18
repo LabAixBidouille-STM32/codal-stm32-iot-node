2ada14e8e4e5832c8fae0fc0876b84cce6f3373e
Sébastien NEDJAR <nedseb@goldaire.com>
2018-11-18 17:51:41 +0200

    Correct the inclusion barrier

--- samples/SerialSample.h
+++ samples/SerialSample.h
@@ -1,8 +1,8 @@
 #include "STM32IotNode.h"
 #include "stm32.h"
 
-#ifndef BLINK_H
-#define BLINK_H
+#ifndef SERIAL_SAMPLE_H
+#define SERIAL_SAMPLE_H
 
 void Serial_main(codal::STM32IotNode& iotNode);
 
