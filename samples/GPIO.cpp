e6e4c85e843870b535eccabe01e9dbf9326d3e6e
Sébastien NEDJAR <nedseb@goldaire.com>
2018-12-12 16:38:47 +0200

    Ajout d'un exemple avec PWM

--- samples/GPIO.cpp
+++ samples/GPIO.cpp
@@ -6,7 +6,8 @@ using namespace codal;
 
 void onSampleEvent(Event e){
     int value = default_device_instance->io.A0.getAnalogValue();
-    printf("A0 = %x\n", value);
+    default_device_instance->io.D6.setAnalogValue(value);
+    printf("A0 = %d\n", value);
 }
 
 
@@ -15,17 +16,16 @@ void GPIO_main(codal::STM32IotNode& iotNode){
     printf("*******************************************\n");
     printf("*           Demonstration de I/O          *\n");
     printf("*******************************************\n");
+    //iotNode.io.A1.setAnalogValue(512);
     iotNode.sleep(1000);
     system_timer_event_every(1000, ID_PIN_P0, SENSOR_UPDATED);
     EventModel::defaultEventBus->listen(ID_PIN_P0, SENSOR_UPDATED, onSampleEvent);
 
     while(1) {
         iotNode.io.led.setDigitalValue(1);
-        iotNode.io.A1.setDigitalValue(1);
         iotNode.sleep(1000);
 
         iotNode.io.led.setDigitalValue(0);
-        iotNode.io.A1.setDigitalValue(0);
         iotNode.sleep(1000);
     }
 }
